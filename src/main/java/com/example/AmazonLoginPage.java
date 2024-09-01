c3dca0b68e112051d23eaecdf23fa86205b31b39
akshit raj patel <32068317+akshitrajpatel@users.noreply.github.com>
2024-09-01 23:30:12 +0530

    Update and rename AmazonLoginTest.java to AmazonLoginPage.java

--- src/main/java/com/example/AmazonLoginPage.java
+++ src/main/java/com/example/AmazonLoginPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class AmazonLoginTest {
+public class AmazonLoginPage {
 
     private WebDriver driver;
 
@@ -21,7 +21,7 @@ public class AmazonLoginTest {
     @FindBy(id = "signInSubmit")
     private WebElement signInButton;
 
-    public AmazonLoginTest(WebDriver driver) {
+    public AmazonLoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
