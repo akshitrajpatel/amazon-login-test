e6724078e76a0c6ffd9152128554155da0edd78a
akshit raj patel <32068317+akshitrajpatel@users.noreply.github.com>
2024-09-05 23:31:59 +0530

    Update AmazonLoginTest.java

--- src/test/java/com/example/AmazonLoginTest.java
+++ src/test/java/com/example/AmazonLoginTest.java
@@ -3,7 +3,7 @@ package com.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -20,12 +20,8 @@ public class AmazonLoginTest {
     public void setup() {
         switch (browser.toLowerCase()) {
             case "firefox":
-                //WebDriverManager.firefoxdriver().setup();
-                //driver = new FirefoxDriver();
-                FirefoxOptions options = new FirefoxOptions();
-                options.setBinary(System.getenv("FIREFOX_HOME")); // Specify the path to firefox.exe
                 WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver(options);
+                driver = new FirefoxDriver();
                 break;
             case "edge":
                 WebDriverManager.edgedriver().setup();
