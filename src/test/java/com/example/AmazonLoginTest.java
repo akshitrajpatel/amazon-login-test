112aeb539ae6ba706d087f4b2d468b9f86ba94f8
akshit raj patel <32068317+akshitrajpatel@users.noreply.github.com>
2024-09-06 12:25:23 +0530

    Update AmazonLoginTest.java

--- src/test/java/com/example/AmazonLoginTest.java
+++ src/test/java/com/example/AmazonLoginTest.java
@@ -1,5 +1,6 @@
 package com.example;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
