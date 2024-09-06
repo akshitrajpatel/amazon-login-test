fd591fcaad180ca5149b36fcc2d8aca0955a5573
akshit raj patel <32068317+akshitrajpatel@users.noreply.github.com>
2024-09-06 12:35:59 +0530

    Update AmazonLoginPage.java

--- src/main/java/com/example/AmazonLoginPage.java
+++ src/main/java/com/example/AmazonLoginPage.java
@@ -29,7 +29,7 @@ public class AmazonLoginPage {
     }
 
     public void navigateTo() {
-        String addUri= "/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0";
+        String addUri= "/";
         driver.get(url + addUri);
     }
 
