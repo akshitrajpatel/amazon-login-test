09d203a71699224cfb85b1feb54fada3775f31c5
akshit raj patel <32068317+akshitrajpatel@users.noreply.github.com>
2024-09-06 12:27:07 +0530

    Update AmazonLoginTest.java

--- src/test/java/com/example/AmazonLoginTest.java
+++ src/test/java/com/example/AmazonLoginTest.java
@@ -39,7 +39,7 @@ public class AmazonLoginTest {
     @Test
     public void testValidLogin() {
         loginPage.navigateTo();
-        WebElement divele = driver.findElement(By.xpath("//div[@id=='ab-signin-link-section']"));
+        WebElement divele = driver.findElement(By.xpath("//div[@id='ab-signin-link-section']"));
         String claaname1=divele.getAttribute("class"); 
         System.out.println("checking element class "+ claaname1);
         // Use XPath to locate the email input field
